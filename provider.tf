b3722ffa4c381f2fe160dd889296e26ac0c6b4d3
baji2110951 <61617189+baji2110951@users.noreply.github.com>
2023-08-29 17:51:03 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,3 +1,12 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
 provider "aws"{
   Region = "ap-south-1"
   access_key = "AKIAVIIWYAHMXQK5WDPY"
