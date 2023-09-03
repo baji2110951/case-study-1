f3e3e1f4ecb44f9f3ce7d4a4ce2f7da1497a8e69
baji2110951 <61617189+baji2110951@users.noreply.github.com>
2023-09-03 18:10:14 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,1 +1,31 @@
 
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+
+
+# Configure the AWS Provider
+provider "aws" {
+  region = "ap-south-1"
+}
+
+
+#ec2 Instance
+
+resource "aws_instance" "myec2" {
+
+  instance_type = "t2.micro"
+
+  ami = "ami-008b85aa3ff5c1b02"
+
+  tags = {
+
+    name = "my_ec2"
+
+  }
+}
