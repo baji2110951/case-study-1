5807cd8216228d92c320556f9e6274cce0a94b01
baji2110951 <61617189+baji2110951@users.noreply.github.com>
2023-09-03 18:07:17 +0530

    Update provider.tf

--- provider.tf
+++ provider.tf
@@ -1,18 +1,1 @@
-provider "aws" {
 
-  region = var.region
-
-}
-
-resource "aws_instance" "myec2" {
-
-     instance_type = var.instance_type
-
-     ami = "ami-008b85aa3ff5c1b02"
-
-     tags = {
-
-     name = "my_ec2"
-
-     }
-}
