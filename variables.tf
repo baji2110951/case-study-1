4c63eacd78f53112a17969380d52968707f024a4
baji2110951 <61617189+baji2110951@users.noreply.github.com>
2023-09-04 15:30:41 +0530

    Update variables.tf

--- variables.tf
+++ variables.tf
@@ -1,11 +1,29 @@
 variable "region" {
 
-   default = "ap-south-1"
+  default = "ap-south-1"
 
 }
 
-variable "instance_type"{
+variable "instance_type" {
 
-   default = "t2.micro"
+  default = "t2.micro"
+
+}
+
+variable "ami" {
+
+  default = "ami-008b85aa3ff5c1b02"
+
+}
+
+variable "access_key" {
+
+  default = "AKIAVIIWYAHM36J32WMA"
+
+}
+
+variable "secret_key" {
+
+  default = "sO9uRcEBKs7fixSdnxWMcVU6Jxw+w/bgxQH3Gu7+"
 
 }
